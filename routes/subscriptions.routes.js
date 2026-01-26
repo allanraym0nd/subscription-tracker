9d0a61686251fb3ef1a0966626a84a76fb3ae4a9
allan raymond <raymonda916@gmail.com>
2026-01-26 19:22:31 +0300

    update subscriptions routes

--- routes/subscriptions.routes.js
+++ routes/subscriptions.routes.js
@@ -2,4 +2,13 @@ import { Router } from "express";
 
 const router = Router()
 
-router.get('/', () => res.send({ title: 'GET all subscriptions' }))
+router.get('/', (req, res) => res.send({ title: 'GET all subscriptions' }))
+router.get('/:id', (req, res) => res.send({ title: 'GET subscription details' }))
+router.post('/', (req, res) => res.send({ title: 'CREATE subscriptions' }))
+router.put('/:id', (req, res) => res.send({ title: 'UPDATE subscription' }))
+router.delete('/:id', (req, res) => res.send({ title: 'DELETE subscription' }))
+router.get('/user/:id', (req, res) => res.send({ title: 'GET all subscription for user' }))
+router.put('/:id/cancel', (req, res) => res.send({ title: 'Cancel user subscription' }))
+router.get('/upcoming-subscription', (req, res) => res.send({ title: 'GET upcoming-subscriptions ' }))
+
+export default router;
