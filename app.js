cc2b9a14193b6bf03af104d2c625f0af5da485dd
allan raymond <raymonda916@gmail.com>
2026-01-26 19:22:31 +0300

    update app entry file

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 import express from 'express'
 import { PORT } from './config/env.js'
+import authRoutes from './routes/auth.routes.js'
+import subscriptionRoutes from './routes/subscriptions.routes.js'
+import userRoutes from './routes/user.routes.js'
+import connectToDatabase from './database/mongodb.js'
+import errorMiddlware from './middleware/error.middleware.js'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
+// Parse body FIRST
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+// THEN add logging
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path}`)
+    console.log('Body:', req.body)
+    next()
+})
+
+// routes
+app.use('/api/v1/auth', authRoutes)
+app.use('/api/v1/users', userRoutes)
+app.use('/api/v1/subscriptions', subscriptionRoutes)
+
+app.use(errorMiddlware)
+
 app.get('/', (req, res) => {
-
-
+    res.send('Hey dont freak out, just finish this up')
 })
 
-app.listen(PORT, () => {
-    console.log("Server Running")
+app.listen(PORT, async () => {
+    console.log(`Server Running on PORT ${PORT}`)
+    await connectToDatabase()
 })
 
-export default app;
+export default app
