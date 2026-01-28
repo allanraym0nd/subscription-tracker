f027b2bf731292d618c4ea15ecd10b4af46486da
allan raymond <raymonda916@gmail.com>
2026-01-28 13:02:39 +0300

    update user routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
 
 const router = Router()
 
-router.get('/', (req, res) => res.send({ title: 'GET all users' }))
-router.get('/:id', (req, res) => res.send({ title: 'GET user details' }))
+router.get('/', getUsers)
+router.get('/:id', getUser)
 router.post('/', (req, res) => res.send({ title: 'CREATE user' }))
 router.put('/:id', (req, res) => res.send({ title: 'UPDATE user' }))
 router.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }))
